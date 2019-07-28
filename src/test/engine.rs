0d1f03dd4ae681ab9cad08f40f4f232c07108925
Ammon Smith <ammon.i.smith@gmail.com>
2019-07-28 02:43:37 -0400

    Add spec test.

--- src/test/engine.rs
+++ src/test/engine.rs
@@ -21,3 +21,37 @@ fn add_remove_tags() {
     assert_eq!(engine.has_tag("test"), true);
     assert_eq!(engine.get_tag("test").unwrap(), tag);
 }
+
+#[test]
+fn add_remove_specs() {
+    let mut engine = Engine::default();
+
+    let mut apple = TemplateTagSpec::default();
+    apple.required_tags.push(Tag::new("banana"));
+
+    let mut banana = TemplateTagSpec::default();
+    banana.needed_roles.push(Role::new("admin"));
+
+    {
+        assert_eq!(engine.has_tag("apple"), false);
+        let tag = engine.add_tag("apple", apple);
+        assert_eq!(engine.has_tag("apple"), true);
+
+        let spec = engine.get_spec(&tag).unwrap();
+        assert_eq!(spec.required_tags.len(), 1);
+    }
+
+    {
+        assert_eq!(engine.has_tag("banana"), false);
+        let tag = engine.add_tag("banana", banana);
+        assert_eq!(engine.has_tag("apple"), true);
+        assert_eq!(engine.has_tag("banana"), true);
+
+        let spec = engine.get_spec(&tag).unwrap();
+        assert_eq!(spec.needed_roles.len(), 1);
+    }
+}
+
+#[test]
+fn add_remove_groups() {
+}
